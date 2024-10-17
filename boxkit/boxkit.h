1ce57eef7c0b7f716efe30db8cdcd24e7cb60a26
Michael Danylchuk <mn_danylchuk@yahoo.com>
2024-10-16 23:50:27 -0700

    Initial Commit

---
+++ boxkit/boxkit.h
@@ -0,0 +1,18 @@
+//
+//  boxkit.h
+//  boxkit
+//
+//  Created by Michael Danylchuk on 10/16/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for boxkit.
+FOUNDATION_EXPORT double boxkitVersionNumber;
+
+//! Project version string for boxkit.
+FOUNDATION_EXPORT const unsigned char boxkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <boxkit/PublicHeader.h>
+
+
